a0ae1e4851667eee101c74f2a1db2767d4cb6119
Beetla Abhinav <104291970+roxbury118@users.noreply.github.com>
2024-10-23 23:33:09 +0530

    Delete ecom_proj/src/main/resources/data.sql

--- ecom_proj/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO PRODUCT (p_name, p_desc, brand, price, catogary, release_Date, available, quant) 
-VALUES 
-  ('Sedan', 'A comfortable and stylish sedan', 'Toyota', 25000.99, 'Car', '2022-01-15', true, 10),
-  ('SUV', 'A spacious and robust SUV', 'Ford', 32000.50, 'Car', '2023-03-10', true, 8),
-  ('Hatchback', 'A compact and efficient hatchback', 'Honda', 18000.75, 'Car', '2021-07-22', true, 15),
-  ('Convertible', 'A luxury convertible sports car', 'BMW', 45000.00, 'Car', '2022-08-30', true, 5),
-  ('Pickup Truck', 'A durable and powerful pickup truck', 'Chevrolet', 38000.85, 'Car', '2023-02-25', true, 12);
